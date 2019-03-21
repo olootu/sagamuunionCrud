1a8b13d1b7fd43ef1061ee257d43f95e47f28a20
Ola Ogunyemi <ola.ogunyemi@deltatre.com>
2019-03-21 13:15:35 +0000

    add search functionality to member list

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,17 @@ import {RouterModule } from '@angular/router';
 import { AngMaterialModule } from '../angMaterial/angMaterial.module';
 import { FormsModule } from '@angular/forms';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SearchPipe } from './pipes/search.pipe';
 
 @NgModule({
-  declarations: [ToolbarComponent],
+  declarations: [ToolbarComponent, SearchPipe],
   imports: [
     CommonModule,
     AngMaterialModule,
     RouterModule
   ],
   exports: [
-    AngMaterialModule, FormsModule, CommonModule, ToolbarComponent
+    AngMaterialModule, FormsModule, CommonModule, ToolbarComponent, SearchPipe
   ]
 })
 export class SharedModule { }
