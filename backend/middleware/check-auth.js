958587bc9a0568a66a6b4d60a94642ef5c341294
Ola Ogunyemi <ola.ogunyemi@deltatre.com>
2019-04-11 13:56:58 +0100

    error correction at server

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try{
-        const token = req.req.authorization.split(' ')[1];
+        const token = req.headers.authorization.split(' ')[1];
         jwt.verify(token, 'secret-this-should-be-longer');
         next();
     } catch (error){
